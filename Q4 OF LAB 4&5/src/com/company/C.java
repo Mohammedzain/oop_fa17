5405710a40a189e4085a2aabde774db968370657
IBAPK\12627 <mohammedzayn97@hotmail.com>
2017-09-21 12:27:41 +0500

    question 4 of lab 4 and 5

---
+++ Q4 OF LAB 4&5/src/com/company/C.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class C extends B{
+    public C(){
+        System.out.println("CONSTRUCTING C");
+    }
+}
---
+++ Q4 OF LAB 4&5/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 A BOX = new A();
+	 B BOX1 = new B();
+	 C BOX2 = new C();
+    }
+}
